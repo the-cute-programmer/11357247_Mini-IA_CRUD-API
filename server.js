4f0c07d42c1af9734ea60d0370f2d4403d7bf7e1
the-cute-programmer <roduronyarko@gmail.com>
2023-12-18 09:25:11 +0000

    final commit

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const apiRoutes = require("../routes/apiRoutes");
 
-const app = express()
+const server = express()
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://localhost:27017",{useNewUrlParser: true, useUnifiedTropoly: true})
+
+connectDB();
+
+app.use('/api', apiRoutes);
 
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+
+
 
 
